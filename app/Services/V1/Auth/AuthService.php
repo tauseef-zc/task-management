a7484ebcfb76fbc798c8c632355c1ba9836de82a
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-17 15:39:15 +0530

    feat: register and forgot password related changes added

--- app/Services/V1/Auth/AuthService.php
+++ app/Services/V1/Auth/AuthService.php
@@ -4,6 +4,7 @@ namespace App\Services\V1\Auth;
 
 use App\Jobs\V1\Auth\VerifyUserEmail;
 use App\Models\User;
+use App\Notifications\V1\Auth\ForgotPasswordNotification;
 use App\Services\V1\BaseService;
 use App\Services\V1\Interfaces\Auth\IAuthService;
 use Illuminate\Http\Response;
@@ -45,4 +46,85 @@ class AuthService extends BaseService implements IAuthService
             return $this->error($th->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
+
+
+    /**
+     * @param array $payload
+     * @return array
+     */
+    public function register(array $payload): array
+    {
+        try {
+
+            $user = $this->user->create($payload);
+            VerifyUserEmail::dispatch($user->email);
+
+            return $this->payload([
+                'message' => 'User created successfully',
+            ], Response::HTTP_CREATED);
+
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), Response::HTTP_BAD_REQUEST );
+        }
+    }
+
+    /**
+     * forgotPassword
+     *
+     * @param string $email
+     * @return array
+     */
+    public function forgotPassword(string $email): array
+    {
+        $user = $this->user->where('email', $email)->first();
+
+        $otp = $user->createOtp();
+        $user->notify(new ForgotPasswordNotification($otp->otp));
+
+        return $this->payload(['message'  => 'Reset password otp sent to your email address']);
+    }
+
+     /**
+     * @param array $payload
+     * @param bool $verifyEmail
+     * @return array
+     */
+    public function verifyOtp(array $payload): array
+    {
+        $user = $this->user->where(['email' => $payload['email']])->first();
+
+        if ($otp = $user->getOtp($payload['otp'])) {
+
+            $user->markOtpAsVerified($otp);
+
+            return $this->payload([
+                'message'  => sprintf('The otp for %s has been verified.', $user->email),
+                'accessToken' => $user->createToken($user->id, ['reset.password'], now()->addMinutes(30))->plainTextToken
+            ]);
+        }
+
+        return $this->error('The otp is incorrect.', Response::HTTP_BAD_REQUEST );
+    }
+
+    /**
+     * @param User $user
+     * @return array
+     */
+    public function verifyUserEmail(array $payload): array
+    {
+        $user = $this->user->where(['email' => $payload['email']])->first();
+
+        if ($otp = $user->getOtp($payload['otp'])) {
+            
+            $user->markOtpAsVerified($otp);
+            $user->markEmailAsVerified();
+
+            return $this->payload([
+                'message'  => sprintf('The email %s has been verified.', $user->email),
+            ]);
+        }
+
+        return $this->error('The otp is incorrect.', Response::HTTP_BAD_REQUEST );
+    }
+
 }
--- app/Services/V1/Interfaces/Auth/IAuthService.php
+++ app/Services/V1/Interfaces/Auth/IAuthService.php
@@ -20,4 +20,37 @@ interface IAuthService extends ServiceInterface
      * @return array
      */
     public function login(array $credentials): array;
+
+    /**
+     * register
+     *
+     * @param  array $data
+     * @return array
+     */
+    public function register(array $data): array;
+
+    /**
+     * forgotPassword
+     * 
+     * @param string $email
+     * @return array
+     */
+    public function forgotPassword(string $email): array;
+
+    /**
+     * verifyOtp
+     * 
+     * @param array $payload
+     * @return array
+     */
+    public function verifyOtp(array $payload): array;
+
+    /**
+     * verifyUserEmail
+     * 
+     * @param array $payload
+     * @return array
+     */
+    public function verifyUserEmail(array $payload): array;
+
 }
