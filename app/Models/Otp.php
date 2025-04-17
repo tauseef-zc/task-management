1ed5a41170b9aa0152424b573d43ac11874f9c20
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-17 15:52:55 +0530

    refactor: missing imports added to the Otp model

--- app/Models/Otp.php
+++ app/Models/Otp.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Otp extends Model
 {
