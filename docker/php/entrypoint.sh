548b55d825570041535ee5a1b49b1a064540b1f7
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-17 14:17:52 +0530

    feat: redis and mail configuration done for auth service

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Wait until MySQL is ready
-# echo "Waiting for MySQL..."
-# until mysqladmin ping -h mysql --silent; do
-#   sleep 2
-# done
-
-# echo "MySQL is up - running Laravel setup..."
-
 # Install dependencies
 composer install
 
@@ -19,9 +11,21 @@ if [ ! -f .env ]; then
   cp .env.example .env
 fi
 
+php artisan config:clear
+php artisan cache:clear
+
 # Generate key and run migrations + seed
 php artisan key:generate
 php artisan migrate:fresh --seed
+
+# Set up storage link
+php artisan storage:link
+
+# Set up queue worker
+php artisan queue:work --daemon --queue=default --sleep=3 --tries=3 &
+
+# Set up scheduler
+php artisan schedule:run --verbose --no-interaction &
 
 # Run PHP-FPM
 php-fpm
