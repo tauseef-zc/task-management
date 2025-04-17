2defbef98059c123b4f48ee31c039633c9d5a1e6
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-17 19:49:24 +0530

    feat: migrations added for task management

---
+++ database/migrations/2025_04_17_135759_create_task_statuses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('color')->default('#000000');
+            $table->string('icon')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_statuses');
+    }
+};
---
+++ database/migrations/2025_04_17_135910_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('status_id')->constrained('task_statuses')->cascadeOnDelete();
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('assigned_to')->nullable()->constrained('task_contributors')->nullOnDelete();
+            $table->foreignId('project_id')->constrained('projects')->cascadeOnDelete();
+            $table->foreignId('parent_id')->nullable()->constrained('tasks')->nullOnDelete();
+            $table->dateTime('due_date')->nullable();
+            $table->string('priority')->default('normal');
+            $table->string('type')->default('task');
+            $table->string('progress')->default('0%');
+            $table->string('estimated_time')->nullable();
+            $table->string('spent_time')->nullable();
+            $table->string('attachments')->nullable();
+            $table->string('comments')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
