e5d0bd4fcdd13cf4cdea6d94e4197fc2bdb4f5dd
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-17 14:21:36 +0530

    feat: api registered for login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+
+    // Auth routes
+    Route::prefix('auth')->name('auth.')->group(function () {
+
+        // Guest Auth routes
+        Route::middleware('guest')->group(function () {
+            Route::post('login', LoginController::class)->name('login');
+        });
+    });
+
+});
