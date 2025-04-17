b7d023456c5bfa99311ebe13d97beca7c131ea12
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-17 15:38:29 +0530

    feat: aut apis registered for register and reset password related routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\{
+    ForgotPasswordController,
+    LoginController,
+    RegisterController,
+    ResetVerifyController,
+    VerificationController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -11,7 +17,14 @@ Route::prefix('v1')->group(function () {
         // Guest Auth routes
         Route::middleware('guest')->group(function () {
             Route::post('login', LoginController::class)->name('login');
+            Route::post('register', RegisterController::class)->name('register');
+            Route::post('email/send-verification', [VerificationController::class, 'sendVerificationMail'])
+                ->name('verification.send');
+            Route::post('verify', [VerificationController::class, 'verifyUser'])->name('verify.user');
+            Route::post('password-verify', [ResetVerifyController::class, 'verifyUser'])->name('verify.user');
+            Route::post('forgot-password', ForgotPasswordController::class)->name('forgot.password');
         });
+
     });
 
 });
