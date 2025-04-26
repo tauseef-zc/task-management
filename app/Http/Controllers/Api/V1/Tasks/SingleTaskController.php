3a780d39c39423e3552c2483986698e384dbb29f
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-26 23:27:34 +0530

    feat: add SingleTaskController for handling individual task retrieval

---
+++ app/Http/Controllers/Api/V1/Tasks/SingleTaskController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Tasks;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\Tasks\TaskResource;
+use App\Models\Task;
+
+class SingleTaskController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Task $task)
+    {
+        $task->load('createdBy', 'assignedTo', 'project', 'status');
+        return TaskResource::make($task);
+    }
+    
+}
