d63f9f281142648d64c4c94c77d51e9f93cf68ee
Tauseef Ahamed <tauseefahamed14@gmail.com>
2025-04-26 22:52:24 +0530

    feat: add HasFilter trait to Task model for enhanced filtering capabilities

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use App\Enums\TaskPriorityEnum;
+use App\Traits\HasFilter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    use HasFilter, HasFactory;
+
     protected $fillable = [
         'name',
         'slug',
